328d10cdb2c6183605b5ad3766928d1e06a06a86
kandirou0 <simo.kandirou15@gmail.com>
2023-03-06 22:00:02 +0100

    update login

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import './assets/index.css'
+import  "flowbite/dist/flowbite.js";
 import router from './router/index'
 
 const app = createApp(App)
