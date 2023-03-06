e8874ca9a08531e998922e70f57f4df772894380
kandirou0 <simo.kandirou15@gmail.com>
2023-03-06 17:32:31 +0100

    update

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,12 +1,26 @@
 <template>
   
   <h1>Index</h1>
+ 
+
+  <input type="text" v-model="form.email"><br>
+  <input type="text" v-model="form.password"><br>
+  <button @click="login">login</button>
 
 </template>
 
 <script setup>
 import axios from 'axios';
-import { onMounted } from 'vue';
+import { onMounted,ref } from 'vue';
+
+const form=ref({
+  email:null,
+  password:null,
+})
+
+function login(){
+  console.log(form.email.value);
+}
 
 onMounted(() => {
   axios.get('http://localhost:8000/sanctum/csrf-cookie')
