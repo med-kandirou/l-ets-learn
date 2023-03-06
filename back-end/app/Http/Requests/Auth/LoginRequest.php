2758843d3d2e7b717d3cb1a164201e92881fb1ce
kandirou0 <simo.kandirou15@gmail.com>
2023-03-06 20:08:56 +0100

    update

--- back-end/app/Http/Requests/Auth/LoginRequest.php
+++ back-end/app/Http/Requests/Auth/LoginRequest.php
@@ -27,7 +27,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => ['required', 'string', 'email'],
+            'email' => ['required', 'string', 'email','unique:users'],
             'password' => ['required', 'string'],
         ];
     }
--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,37 +1,40 @@
 <template>
-  
   <h1>Index</h1>
- 
+
 
   <input type="text" v-model="form.email"><br>
   <input type="text" v-model="form.password"><br>
   <button @click="login">login</button>
 
   {{ user }}
-
 </template>
 
 <script setup>
 import axios from 'axios';
-import { onMounted,ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
-const form=ref({
-  email:null,
-  password:null,
+const form = ref({
+  email: null,
+  password: null,
 })
-const user=ref();
-async function login(){
-  axios.defaults.withCredentials=true;
+const user = ref();
+async function login() {
+  axios.defaults.withCredentials = true;
   await axios.get('http://localhost:8000/sanctum/csrf-cookie')
-  await axios.post('http://localhost:8000/login',{
-    email:form.value.email,
-    password:form.value.password
-  })
+  try {
+    await axios.post('http://localhost:8000/login', {
+      email: form.value.email,
+      password: form.value.password
+    })
+  } catch (error) {
+    console.log(error);
+  }
 
-  await axios.get('http://localhost:8000/api/user')
-  .then(res=>{
-    user.value=res.data
-  })
+
+  // await axios.get('http://localhost:8000/api/user')
+  // .then(res=>{
+  //   user.value=res.data
+  // })
 
 }
 
