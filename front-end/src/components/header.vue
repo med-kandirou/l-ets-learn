13ae1bd22d6d835c4309302a4e50c29143eaa228
kandirou0 <simo.kandirou15@gmail.com>
2023-03-07 16:55:27 +0100

    update

--- front-end/src/components/header.vue
+++ front-end/src/components/header.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <nav class="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+    <nav class="bg-white px-2 sm:px-4 py-2.5 bg-inherit dark:bg-gray-900 w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
         <div class="container flex flex-wrap items-center justify-between mx-auto">
         <a href="https://flowbite.com/" class="flex items-center">
             <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo">
