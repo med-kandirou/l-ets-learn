82dacf46f87f0720b73a37264a4b2dbbf3cc865b
kandirou0 <simo.kandirou15@gmail.com>
2023-03-07 19:40:47 +0100

    update

--- front-end/src/guest/signup.vue
+++ front-end/src/guest/signup.vue
@@ -31,7 +31,7 @@
             </div>
             <div class="relative">
               <p class="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600 absolute">Email</p>
-              <input placeholder="123@ex.com" type="text" class="border placeholder-gray-400 focus:outline-none
+              <input placeholder="email@exemple.com" type="text" class="border placeholder-gray-400 focus:outline-none
                   focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
                   border-gray-300 rounded-md"/>
             </div>
@@ -44,8 +44,8 @@
             </div>
             <div class="relative">
               <p class="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
-                  absolute">Confirm password</p>
-              <input placeholder="Password" type="password" class="border placeholder-gray-400 focus:outline-none
+                  absolute">Confirmation password</p>
+              <input placeholder="Confirmation" type="password" class="border placeholder-gray-400 focus:outline-none
                   focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
                   border-gray-300 rounded-md"/>
             </div>
