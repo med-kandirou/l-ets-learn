6eee0d6493b1d9ee2c4eb83fdd4134ca3112fd44
kandirou0 <simo.kandirou15@gmail.com>
2023-03-09 11:14:49 +0100

    update

--- front-end/src/components/header.vue
+++ front-end/src/components/header.vue
@@ -7,13 +7,13 @@
             <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
         </a>
         <div class="flex md:order-2">
-            <router-link to="/login" class="inline-flex items-center justify-center p-0.5 mb-2 mr-1 mt-2 overflow-hidden text-sm font-medium text-blue-700 rounded-lg group bg-gradient-to-br from-blue-600 bg-blue-600 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+            <router-link to="/login" class="inline-flex items-center justify-center p-0.5 mb-2 mr-1 mt-2 overflow-hidden text-sm font-medium text-blue-700 rounded-lg group bg-gradient-to-br from-blue-600 bg-blue-600  group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
                 <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                     Se connecter
                 </span>
             </router-link>
-            <router-link to="/signup" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 mt-2 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-blue-600 bg-blue-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-blue-600 dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            <router-link to="/signup" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 mt-2 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-blue-600 bg-blue-600 group-hover:to-blue-500 hover:text-blue-600 hover:bg-white  dark:text-white dark:focus:ring-blue-800">
+                <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-blue-600   dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                     S'inscrire
                 </span>
             </router-link>
@@ -25,7 +25,7 @@
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
             <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-                <router-link to="/" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</router-link>
+                <router-link to="/" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Principale</router-link>
             </li>
             <li>
                 <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Exemple</a>
