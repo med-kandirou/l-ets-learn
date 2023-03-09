d333da43635446be5bd1fa5fff8aedbe1cda56f9
kandirou0 <simo.kandirou15@gmail.com>
2023-03-09 16:47:50 +0100

    update

--- front-end/src/components/header.vue
+++ front-end/src/components/header.vue
@@ -45,9 +45,9 @@
 
 <script >
     import { initCarousels, initCollapses, initDials, initDismisses, initDrawers, initDropdowns, initModals, initPopovers, initTabs, initTooltips } from 'flowbite'
-    import {userStore} from "@/stores/user";
+    // import {userStore} from "../stores/user.js";
     import { onMounted,ref } from 'vue';
-    // const user=userStore();
+    const user=userStore();
 
     onMounted(() => {
         initCarousels();
