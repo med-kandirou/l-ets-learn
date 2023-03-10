a8e50332b89cf3f29d8a9b8f9179808da19f1c9b
kandirou0 <simo.kandirou15@gmail.com>
2023-03-10 11:46:21 +0100

    update

--- front-end/src/components/header.vue
+++ front-end/src/components/header.vue
@@ -7,16 +7,22 @@
             <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
         </a>
         <div class="flex md:order-2">
-            <router-link to="/login" class="inline-flex items-center justify-center p-0.5 mb-2 mr-1 mt-2 overflow-hidden text-sm font-medium text-blue-700 rounded-lg group bg-gradient-to-br from-blue-600 bg-blue-600  group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                    Se connecter
-            </span>
-            </router-link>
-            <router-link to="/signup" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 mt-2 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-blue-600 bg-blue-600 group-hover:to-blue-500 hover:text-blue-600 hover:bg-white  dark:text-white dark:focus:ring-blue-800">
-                <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-blue-600   dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                    S'inscrire
-                </span>
-            </router-link>
+            <div v-if="islogged">
+                profile
+            </div>
+            <div v-else>
+                <router-link to="/login" class="inline-flex items-center justify-center p-0.5 mb-2 mr-1 mt-2 overflow-hidden text-sm font-medium text-blue-700 rounded-lg group bg-gradient-to-br from-blue-600 bg-blue-600  group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                    <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        Se connecter
+                    </span>
+                </router-link>
+                <router-link to="/signup" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 mt-2 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-blue-600 bg-blue-600 group-hover:to-blue-500 hover:text-blue-600 hover:bg-white  dark:text-white dark:focus:ring-blue-800">
+                    <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-blue-600   dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        S'inscrire
+                    </span>
+                </router-link>
+            </div>
+
             <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
@@ -39,8 +45,7 @@
             </ul>
         </div>
         </div>
-
-        {{ user }}
+        
     </nav> 
 </template>
 
@@ -50,9 +55,9 @@
     import { userStore } from "@/stores/user.js";
     import { onMounted,ref } from 'vue';
     const user=userStore();
-    
-   
+    const islogged=ref(false);
     onMounted(() => {
+        islogged.value=user.checkifUser();
         initCarousels();
         initCollapses();
         initDials();
