bd2309c192bc11c8b95b277dc4765f6a8c830bc6
kandirou0 <simo.kandirou15@gmail.com>
2023-03-11 16:12:54 +0100

    update

--- front-end/src/components/cour.vue
+++ front-end/src/components/cour.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="max-w-sm mb-7 bg-gray-100 border shadow-xl hover:border-red-700 border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+    <div class="max-w-sm mb-7 bg-gray-100 border shadow-xl group hover:border-red-700 border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
             <img class="rounded-t-lg" src="/images/banner.png" alt="image" />
         </a>
         <div class="p-5">
             <a href="#">
-                <h6 class="mb-2 text-xl font-bold tracking-tight text-blue-900 dark:text-white">WordPress avancé: maîtrisez les fonctionnalités avancées du CMS Wordpress</h6>
+                <h6 class="mb-2 text-xl font-bold tracking-tight text-blue-900 dark:text-white group-hover:text-red-500">WordPress avancé: maîtrisez les fonctionnalités avancées du CMS Wordpress</h6>
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Créez des sites Web complexes avec WordPress</p> 
+            <p class="mb-3 font-normal text-blue-500 dark:text-gray-400">Créez des sites Web complexes avec WordPress</p> 
         </div>
         <div class="flex justify-around mb-7">
             <div class="flex">
                 <img src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" class="w-10 h-10 rounded-full">
-                <p class="ml-2 mt-auto mb-auto">Mohamed Kandirou</p>
+                <p class="ml-2 mt-auto mb-auto font-light">Mohamed Kandirou</p>
             </div>
             <div>
                 <p class="text-red-500 font-extrabold"><span class="text-2xl">50 </span>MAD</p>
