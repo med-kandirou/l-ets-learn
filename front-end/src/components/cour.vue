729fa2fe0a09341cce80f22bd50e33d0c90b9f78
kandirou0 <simo.kandirou15@gmail.com>
2023-03-11 13:17:26 +0100

    update

---
+++ front-end/src/components/cour.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img class="rounded-t-lg" src="/images/banner.png" alt="" />
+    </a>
+    <div class="p-5">
+        <a href="#">
+            <h6 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">WordPress avancé: maîtrisez les fonctionnalités avancées du CMS Wordpress</h6>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Créez des sites Web complexes avec WordPress</p> 
+    </div>
+    <div class="flex justify-around">
+        <div class="flex">
+            <img src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" class="w-10 h-10 rounded-full">
+            <p class="mt-auto mb-auto">formateur</p>
+        </div>
+        <div>
+            <p class="text-red-500"><span>$</span>50</p>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
