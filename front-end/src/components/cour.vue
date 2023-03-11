881cc5c2b824e95f398a908005a23cc0af14e4ec
kandirou0 <simo.kandirou15@gmail.com>
2023-03-11 13:27:39 +0100

    add card

--- front-end/src/components/cour.vue
+++ front-end/src/components/cour.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img class="rounded-t-lg" src="/images/banner.png" alt="" />
-    </a>
-    <div class="p-5">
         <a href="#">
-            <h6 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">WordPress avancé: maîtrisez les fonctionnalités avancées du CMS Wordpress</h6>
+            <img class="rounded-t-lg" src="/images/banner.png" alt="" />
         </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Créez des sites Web complexes avec WordPress</p> 
-    </div>
-    <div class="flex justify-around">
-        <div class="flex">
-            <img src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" class="w-10 h-10 rounded-full">
-            <p class="mt-auto mb-auto">formateur</p>
+        <div class="p-5">
+            <a href="#">
+                <h6 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">WordPress avancé: maîtrisez les fonctionnalités avancées du CMS Wordpress</h6>
+            </a>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Créez des sites Web complexes avec WordPress</p> 
         </div>
-        <div>
-            <p class="text-red-500"><span>$</span>50</p>
+        <div class="flex justify-around mb-7">
+            <div class="flex">
+                <img src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" class="w-10 h-10 rounded-full">
+                <p class="ml-2 mt-auto mb-auto">Mohamed Kandirou</p>
+            </div>
+            <div>
+                <p class="text-red-500 font-bold"><span class="text-2xl">50</span>MAD</p>
+            </div>
         </div>
-    </div>
     </div>
 </template>
 
