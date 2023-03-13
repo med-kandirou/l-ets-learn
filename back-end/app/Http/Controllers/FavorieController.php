ff8ddc5be1f982d62373b7ad0c10d3df6e338948
kandirou0 <simo.kandirou15@gmail.com>
2023-03-13 19:23:48 +0100

    updqte

--- back-end/app/Http/Controllers/FavorieController.php
+++ back-end/app/Http/Controllers/FavorieController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class FavorieController extends Controller
 {
-    //
+    function getfavoriebyid($id){
+        $user=User::find($id);
+        return $user->cours;  
+    }
 }
--- front-end/src/views/course.vue
+++ front-end/src/views/course.vue
@@ -7,7 +7,7 @@
             <p class="mb-8 text-lg font-normal text-white lg:text-2xl sm:px-16 xl:px-48 dark:text-gray-400">Créez des sites Web complexes avec WordPress</p>
             <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                 <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                    Ajouter au pannier
+                    Ajouter au favories
                 </a> 
                 <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                     <svg class="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
