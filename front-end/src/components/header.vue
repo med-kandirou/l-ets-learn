e0a2432b8bc31e35abbf5fb0da9a180dafcbbd5c
kandirou0 <simo.kandirou15@gmail.com>
2023-03-17 11:53:22 +0100

    update

--- front-end/src/components/header.vue
+++ front-end/src/components/header.vue
@@ -40,7 +40,7 @@
                 <router-link to="/signup" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Exemple </router-link>
             </li>
             <li>
-                <router-link to="" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Devenir formateur</router-link>
+                <router-link to="/devenirformateur" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Devenir formateur</router-link>
             </li>
             </ul>
         </div>
