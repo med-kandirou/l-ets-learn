3b91d90bb98a969974bf8817ddd2fad60009c9fd
kandirou0 <simo.kandirou15@gmail.com>
2023-03-20 15:19:56 +0000

    update

--- front-end/src/components/videoTitle.vue
+++ front-end/src/components/videoTitle.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex items-center justify-center mb-3">
+    <div class="w-full flex items-center justify-center p-4 border border-slate-200 ">
         <div class="w-7 h-7 border-2 rounded-full border-red-500"></div>
         <div class="w-7 h-7 border-r-2 border-indigo-500"></div>
         <div class="flex items-center">
