7dc7cd1dd6f6e0a69f8afec11fd638e8767b1cd6
kandirou0 <simo.kandirou15@gmail.com>
2023-03-20 15:15:12 +0000

    update

---
+++ front-end/src/components/videoTitle.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex items-center justify-center mb-3">
+        <div class="w-7 h-7 border-2 rounded-full border-red-500"></div>
+        <div class="w-7 h-7 border-r-2 border-indigo-500"></div>
+        <div class="flex items-center">
+            <img src="/images/video.png" class="mr-3 ml-4 h-9" alt="video icon">
+            <div class="taille">Cour 1 (23:07)</div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.taille{
+    color: #003679;
+}
+</style>
--- front-end/src/views/watch.vue
+++ front-end/src/views/watch.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-full grid grid-cols-[auto_1fr] ">
-        <div class="w-96 h-screen border-r-2 border-withe">
+        <div class="w-96 h-screen border-r-2 border-withe bg-gray-100">
             <div class="header h-20 flex justify-between items-center">
                 <button class="ml-2">
                     <img src="/images/home.png" class="h-10">
@@ -9,7 +9,20 @@
                     <img src="/images/settings.png" class="h-10">
                 </button>
             </div>
-            <h1 class="mx-auto w-4/5 mt-4 mb-4 text-xl font-normal tracking-tight leading-none text-black md:text-xl lg:text-xl dark:text-white">Tout ce que vous devez savoir sur les Hooks et le Redux Toolkit avec ReactJS 2022</h1>
+            <div class="">
+                <h1 class="mx-auto w-4/5 mt-4 mb-8 text-xl font-normal tracking-tight leading-none text-black md:text-xl lg:text-xl dark:text-white">Tout ce que vous devez savoir sur les Hooks et le Redux Toolkit avec ReactJS 2022</h1>
+                <videoTitle />
+                <videoTitle />
+                <videoTitle />
+                <videoTitle />
+                <videoTitle />
+                <videoTitle />
+                <videoTitle />
+                <videoTitle />
+                <videoTitle />
+                
+            </div>
+            
         </div>
         <div class="w-full">
             <div class="header h-20 flex justify-end items-center">
@@ -23,10 +36,21 @@
                 <video controls class="video w-4/5 h-2/5">
                     <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4">
                 </video>
+
+                <button class="btn text-white bg-red-500 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                    Complete and continue
+                    <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
             </div>
         </div>
     </div>
 </template>
+
+<script setup>
+
+import videoTitle from '@/components/videoTitle.vue'
+
+</script>
 
 <style scoped>
 .header{
@@ -35,7 +59,9 @@
 .video {
   display: block;
   margin: auto;
-  /* margin-top: 15%;
-  margin-bottom: 20%; */
+}
+.btn{
+    display: block;
+    margin: auto;
 }
 </style>
