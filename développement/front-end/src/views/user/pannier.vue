c28840ced0180a19b3d00d1542c813eb4dedae99
kandirou0 <simo.kandirou15@gmail.com>
2023-03-23 12:50:03 +0000

    ramadan kareem

--- développement/front-end/src/views/user/pannier.vue
+++ développement/front-end/src/views/user/pannier.vue
@@ -1,8 +1,10 @@
 <template>
     <Header />
+    <h1 class="text-center mb-4 mt-10 text-3xl font-bold leading-none text-black dark:text-white">Mes favories</h1>
 
 </template>
 
 <script setup>
+
 import Header from '@/components/header.vue'
 </script>
