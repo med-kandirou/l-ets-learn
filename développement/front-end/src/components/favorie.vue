9f13d84bf9a6094f0c93c39164d87c508c56cba7
kandirou0 <simo.kandirou15@gmail.com>
2023-03-27 16:27:06 +0200

    add favories

--- développement/front-end/src/components/favorie.vue
+++ développement/front-end/src/components/favorie.vue
@@ -1,7 +1,7 @@
 <template>
-    <div @click="show(id)" class="max-w-sm mb-7 bg-gray-100 border shadow-xl group hover:border-red-700 border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+    <div class="max-w-sm mb-7 bg-gray-100 border shadow-xl group hover:border-red-700 border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-            <img class="rounded-t-lg" :src="image" alt="image" />
+            <img @click="show(id)" class="rounded-t-lg" :src="image" alt="image" />
         </a>
         <div class="p-4">
             <a href="#">
@@ -12,29 +12,29 @@
         <div class="flex justify-around mb-7">
             <div class="flex">
                 <img src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" class="w-10 h-10 rounded-full">
-                <p class="ml-2 mt-auto mb-auto font-light">{{nom_user}} {{prenom_user}}</p>
+                <p class="ml-2 mt-auto mb-auto font-light">{{nom_formateur}} {{prenom_formateur}}</p>
             </div>
             <div>
                 <p class="text-red-500 font-extrabold"><span class="text-2xl">{{ price }} </span>MAD</p>
             </div>
         </div>
+        <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Supprimer</button>
     </div>
 </template>
 
 <script setup>
-    import router from '@/router/index';
+import router from '@/router/index';
 defineProps({
     id:Number,
     title: String,
     subtitle: String,
     image: String,
     price: Number,
-    nom_user: String,
-    prenom_user: String,
+    nom_formateur: String,
+    prenom_formateur: String,
 })
 
 function show(id){
     router.push('/course/'+id+'')
 }
-
 </script>
--- développement/front-end/src/views/user/favories.vue
+++ développement/front-end/src/views/user/favories.vue
@@ -2,19 +2,23 @@
     <Header />
     <h1 class="text-center mb-4 mt-10 text-3xl font-bold leading-none text-black dark:text-white">Mes favories</h1>
 
-    <div v-for="favorie in favories">
-        
+    
+
+    <div class="grid grid-cols-1 gap-6 justify-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
+        <div v-for="favorie in favories">
+            <Favorie @show="show" :id="favorie.pivot.cour_id" :title="favorie.title" :subtitle="favorie.subtitle" :image="favorie.image" :price="favorie.price" :nom_formateur="favorie.formateur.nom" :prenom_formateur="favorie.formateur.prenom" />
+        </div>
     </div>
 
 </template>
 
 <script setup>
 import Header from '@/components/header.vue'
+import Favorie from '@/components/favorie.vue'
 import {getFavories} from '@/api/user.js'
 import { userStore } from "@/stores/user.js";
 import { onMounted,ref } from 'vue';
 const user=userStore();
-
 const favories=ref();
 
 onMounted(()=>{
