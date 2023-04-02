54e39a6820c48d76f789f961b6678b6603ecb4c3
kandirou0 <simo.kandirou15@gmail.com>
2023-04-02 18:14:46 +0200

    login formateur

--- développement/front-end/src/views/signupform.vue
+++ développement/front-end/src/views/signupform.vue
@@ -81,11 +81,11 @@
                     </div>
                     <div class="col-span-6 sm:col-span-4">
                         <label class="block text-sm font-medium text-gray-700">Password</label>
-                        <input type="text" v-model="formateur.password" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                        <input type="password" v-model="formateur.password" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                     </div>
                     <div class="col-span-6 sm:col-span-4">
                         <label class="block text-sm font-medium text-gray-700">Confirmation Password</label>
-                        <input type="text" v-model="formateur.confirmation" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                        <input type="password" v-model="formateur.confirmation" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                     </div>
                 </div>
             </div>
