cfae97a440676ecc43e4abdc666280559af74ec8
kandirou0 <simo.kandirou15@gmail.com>
2023-04-04 20:00:25 +0200

    update

--- développement/front-end/src/views/formateur/index.vue
+++ développement/front-end/src/views/formateur/index.vue
@@ -213,20 +213,6 @@
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
 
-import { initCarousels, initCollapses, initDials, initDismisses, initDrawers, initDropdowns, initModals, initPopovers, initTabs, initTooltips } from 'flowbite'
-onMounted(() => {
-        initCarousels();
-        initCollapses();
-        initDials();
-        initDismisses();
-        initDrawers();
-        initDropdowns();
-        initModals();
-        initPopovers();
-        initTabs();
-        initTooltips();
-    })
 
 </script>
