7c7ad4300b33a0e4df46f6c7fc8a9810b346e63d
kandirou0 <simo.kandirou15@gmail.com>
2023-04-07 00:32:16 +0200

    update

--- développement/back-end/app/Http/Controllers/UsersController.php
+++ développement/back-end/app/Http/Controllers/UsersController.php
@@ -3,25 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Cour;
-use App\Models\Formateur;
 
-class CourController extends Controller
+class UsersController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //get product with creator
-        return Cour::with('formateur:id,nom,prenom')->get(['id','title','subtitle','image','price','formateur_id']);
+        //
     }
 
-    function etudiantParCour($id_c){
-        //get les etudiant d'un cour
-        $cour=Cour::find($id_c);
-        return $cour->users()->get();//ici
-    }
     /**
      * Show the form for creating a new resource.
      */
@@ -43,7 +35,7 @@ class CourController extends Controller
      */
     public function show(string $id)
     {
-        return Cour::where('id', $id)->with('formateur')->get();
+        //
     }
 
     /**
@@ -59,14 +51,7 @@ class CourController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        $cour=Cour::find($id);
-        $cour->title=$request->title;
-        $cour->subtitle=$request->subtitle;
-        $cour->base=$request->base;
-        $cour->but=$request->but;
-        $cour->price=$request->price;
-        $cour->update();
-        return 'update';
+        //
     }
 
     /**
@@ -76,12 +61,4 @@ class CourController extends Controller
     {
         //
     }
-
-
-    public function getVideos(string $cour_id)
-    {
-        $cour=Cour::find($cour_id); 
-        return $cour->videos()->get();
-    }
-
 }
