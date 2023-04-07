019c74a734be99781926f3ffc227be809a9eb89a
kandirou0 <simo.kandirou15@gmail.com>
2023-04-07 13:44:43 +0200

    update

--- développement/back-end/database/migrations/2014_10_12_000000_create_users_table.php
+++ développement/back-end/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('image')->nullable();
             $table->string('nom');
             $table->string('prenom');
             $table->string('email')->unique();
