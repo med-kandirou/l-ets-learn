b0f570a99704c671a710bbd1d099fc580da2a194
kandirou0 <simo.kandirou15@gmail.com>
2023-04-08 19:09:03 +0200

    update

--- développement/front-end/src/views/formateur/mesEtudiant.vue
+++ développement/front-end/src/views/formateur/mesEtudiant.vue
@@ -1,7 +1,7 @@
 <template>
     <Dashbord title="Mes etudiants">
-        <div>
-            <label for="cours" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Selectionnez un
+        <div class="mb-11">
+            <label for="cours" class="block mb-11 text-sm font-medium text-gray-900 dark:text-white">Selectionnez un
                 cour</label>
             <select @change="getEtudiants" v-model="id_cour" id="cours"
                 class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
@@ -32,7 +32,7 @@
                     
                     <tr class="border-b border-gray-200 dark:border-gray-700" v-for="etuadiant in etuadiants">
                         <th scope="row" class="px-6 py-4 text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                            <img :src="etuadiant.image" alt="pic user">
+                            <img :src="etuadiant.image" class="rounded-full w-32 h-32" alt="pic user">
                         </th>
                         <td class="px-6 py-4">
                             {{etuadiant.nom}}
