6ab2938d08b9d69278fadfde4165c91c42b13af4
kandirou0 <simo.kandirou15@gmail.com>
2023-04-11 20:35:12 +0000

    update

--- développement/front-end/src/views/admin/etudiant.vue
+++ développement/front-end/src/views/admin/etudiant.vue
@@ -3,7 +3,7 @@
         <div class="mt-10 relative overflow-x-auto shadow-md sm:rounded-lg">
             <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <caption class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
-                    Le nombre des etudiant dans la platforme est :({{ total }}) 
+                    Le nombre des etudiant dans la platforme est :  ({{ total }}) 
                 </caption>
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr >
@@ -42,7 +42,7 @@
                             {{etudiant.course_count}}
                         </td>
                         <td class="px-6 py-4 text-right">
-                            <button class="font-medium text-blue-600 dark:text-blue-500 hover:underline">voir plus</button>
+                            <button @click="details(etudiant.id)" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">voir plus</button>
                         </td>
                     </tr>
                 </tbody>
@@ -64,5 +64,8 @@
         let res=await getAllEtudiant();
         etudiants.value=res;
         total.value=res.length;
+    }
+    async function details(){
+       
     }
 </script>
