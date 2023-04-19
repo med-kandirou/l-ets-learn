2f96ae179e3d2272719f51b831efecc45c7d6992
kandirou0 <simo.kandirou15@gmail.com>
2023-04-19 15:45:51 +0000

    from youssoufia

--- développement/back-end/database/migrations/2023_03_12_000000_create_users_table.php
+++ développement/back-end/database/migrations/2023_03_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->integer('role');
+            
         });
     }
 
