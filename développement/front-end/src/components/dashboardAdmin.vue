c421bfffeab3fc511b1b3b7b3e93a4bcef2b3be7
kandirou0 <simo.kandirou15@gmail.com>
2023-04-24 13:07:21 +0100

    add info admin

--- développement/front-end/src/components/dashboardAdmin.vue
+++ développement/front-end/src/components/dashboardAdmin.vue
@@ -104,16 +104,17 @@
 
           <!-- Profile dropdown -->
           <div class="ml-3 relative">
-            <div>
+            <div class="flex">
               <button type="button" class="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                 <span class="sr-only">Open user menu</span>
                 <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
               </button>
+              <div class="text-gray-500 text-sm">
+                <p>{{ user.nom }} {{ user.prenom }}</p>
+                <p>{{ user.email }}</p>
+              </div>
             </div>
-            <!-- <div class="origin-top-right absolute hidden right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-              <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-              <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
-            </div> -->
+          
           </div>
         </div>
       </div>
@@ -139,6 +140,8 @@
 <script setup>
   import router from '@/router/index'
   import {deconnecter} from '@/api/user.js'
+  import { userStore } from "@/stores/user";
+  const user=userStore();
   defineProps({
     title:String
   })
