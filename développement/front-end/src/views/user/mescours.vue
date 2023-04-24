5220edd64a0a484340b243264b611e692b3eb10d
kandirou0 <simo.kandirou15@gmail.com>
2023-04-24 18:48:36 +0100

    update

--- développement/front-end/src/views/user/mescours.vue
+++ développement/front-end/src/views/user/mescours.vue
@@ -2,9 +2,28 @@
     <Header />
     <h1 class="text-center mb-11 mt-10 text-3xl font-bold leading-none text-black dark:text-white">Ma collection</h1>
 
-    
-   
-
+    <div class="grid grid-cols-1 gap-6 justify-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
+        <div v-for="cour in couses" @click="" class="max-w-sm mb-7 bg-gray-100 border shadow-xl group hover:border-red-700 border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <img class="rounded-t-lg" :src="cour.image" alt="image" />
+            </a>
+            <div class="p-4">
+                <a href="#">
+                    <h6 class="mb-2 text-xl font-bold tracking-tight text-blue-900 dark:text-white group-hover:text-red-500">{{cour.title}}</h6>
+                </a>
+                <p class="mb-3 font-normal text-blue-500 dark:text-gray-400">{{cour.subtitle}}</p> 
+            </div>
+            <div class="flex justify-around mb-7">
+                <div class="flex">
+                    <img src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" class="w-10 h-10 rounded-full">
+                    <p class="ml-2 mt-auto mb-auto font-light">{{cour.nom_user}} {{cour.prenom_user}}</p>
+                </div>
+                <div>
+                    <p class="text-red-500 font-extrabold"><span class="text-2xl">{{ cour.price }} </span>MAD</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
@@ -18,6 +37,7 @@ const couses=ref();
 onMounted(()=>{
     getCours(user.id).then(res=>{
         couses.value=res;
+        console.log(couses.value)
     })
 })
 
