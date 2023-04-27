374f7efae45ece59f37c4a1397b3c8e386866518
kandirou0 <simo.kandirou15@gmail.com>
2023-04-27 23:56:22 +0100

    update

--- développement/front-end/src/views/formateur/index.vue
+++ développement/front-end/src/views/formateur/index.vue
@@ -1,6 +1,52 @@
 <template>
     <Dashboard title="Dashboard">
-        index page
+        <div class="text-center">
+            <h4 class="text-xl font-medium text-gray-900">Statistiques du platform</h4>
+        </div>
+
+        <div class="grid grid-cols-1 gap-6 px-6 mt-8 sm:px-0 lg:mt-16 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-12">
+            <div class="overflow-hidden bg-white border border-gray-200 rounded-lg">
+                <div class="px-4 py-6">
+                    <div class="flex items-start">
+                        <svg class="flex-shrink-0 w-12 h-12 text-fuchsia-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                        </svg>
+                        <div class="ml-4">
+                            <h4 class="text-4xl font-bold text-gray-900">2</h4>
+                            <p class="mt-1.5 text-lg font-medium leading-tight text-gray-500">Nombre des cours</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="overflow-hidden bg-white border border-gray-200 rounded-lg">
+                <div class="px-4 py-6">
+                    <div class="flex items-start">
+                        <svg class="flex-shrink-0 w-12 h-12 text-fuchsia-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+                        </svg>
+                        <div class="ml-4">
+                            <h4 class="text-4xl font-bold text-gray-900">6</h4>
+                            <p class="mt-1.5 text-lg font-medium leading-tight text-gray-500">Nombre des etudiant</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="overflow-hidden bg-white border border-gray-200 rounded-lg">
+                <div class="px-4 py-6">
+                    <div class="flex items-start">
+                        <svg class="flex-shrink-0 w-12 h-12 text-fuchsia-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                        </svg>
+                        <div class="ml-4">
+                            <h4 class="text-4xl font-bold text-gray-900">4</h4>
+                            <p class="mt-1.5 text-lg font-medium leading-tight text-gray-500">Nombre des video</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </Dashboard>
 </template>
 
